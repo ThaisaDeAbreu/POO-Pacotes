b19a9341eefef146778cc62e6c2a42651f6b3dfb
Theily <112132184+Theily@users.noreply.github.com>
2023-03-13 10:57:19 -0300

    Add files via upload

--- Contribuinte/Informa_Dados.java
+++ Contribuinte/Informa_Dados.java
@@ -1,22 +1,30 @@
 package Contribuinte;
 
+
+import javax.swing.JOptionPane;
 public class Informa_Dados {
-	
-	public static void main(String args[]) {
+public static void main(String args[]) {
 		
 		Dados_Contribuinte a = new Dados_Contribuinte();
 		
-		a.setNome("Thaisa de Abreu Souza");
-		a.setCPF("000.000.000-00");
-		a.setCNPJ("111 111 /1111");
-		a.setCartao("2222/2");
-				
+		String n, cpf, cnpj, cart;
+		
+		n = JOptionPane.showInputDialog("Digite o seu nome:");
+		a.setNome(n);
+		
+		cpf = JOptionPane.showInputDialog("Digite o seu CPF:");
+		a.setCPF(cpf);
+		
+		cnpj = JOptionPane.showInputDialog("Digite o seu CNPJ:");
+		a.setCNPJ(cnpj);
+		
+		cart = JOptionPane.showInputDialog("Digite o numero do seu cartao:");
+		a.setCartao(cart);
 		
 		System.out.println("O nome do contribuinte é: " + a.getNome());
 		System.out.println("O CPF do contribuinte é: " + a.getCPF());
 		System.out.println("O CNPJ do contribuinte é: " + a.getCNPJ());
 		System.out.println("O n. do Cartao do contribuinte é: " + a.getCartao());
-					
-	}
-
+		
+		}
 }
